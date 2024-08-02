5789ca9d26c51473df12ad4800b862ac63f608f1
tomihartanto <tomi.hartanto@hatchwealth.com>
2024-08-02 17:36:08 +0700

    update home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,10 @@ import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:healthypet/models/doctor_model.dart';
+import 'package:healthypet/models/service_model.dart';
+
+var selectedService = 0;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,11 +19,218 @@ class HomeScreen extends StatelessWidget {
           child: Column(
         children: [
           const SizedBox(
-            height: 22,
+            height: 20,
           ),
-          _greetings()
+          _greetings(),
+          const SizedBox(
+            height: 16,
+          ),
+          _card(),
+          const SizedBox(
+            height: 20,
+          ),
+          _search(),
+          const SizedBox(
+            height: 20,
+          ),
+          _services(),
+          const SizedBox(
+            height: 27,
+          ),
+          _doctors()
         ],
       )),
+    );
+  }
+
+  ListView _doctors() {
+    return ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemBuilder: (context, index) => _doctor(doctors[index]),
+        separatorBuilder: (context, index) => const SizedBox(
+              height: 11,
+            ),
+        itemCount: doctors.length);
+  }
+
+  Container _doctor(DoctorModel doctorModel) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+          boxShadow: [
+            BoxShadow(
+                color: const Color(0xFF35385A).withOpacity(.12),
+                blurRadius: 30,
+                offset: const Offset(0, 2))
+          ]),
+      child: Row(
+        children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+            child: Image.asset(
+              'assets/images/${doctorModel.image}',
+              width: 88,
+              height: 103,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                doctorModel.name,
+                style: GoogleFonts.manrope(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFF3F3F3F)),
+              ),
+              const SizedBox(
+                width: 7,
+              ),
+              RichText(
+                  text: TextSpan(
+                      text: "Service: ${doctorModel.services.join(', ')}",
+                      style: GoogleFonts.manrope(
+                          fontSize: 12, color: Colors.black)))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  SizedBox _services() {
+    return SizedBox(
+      height: 36,
+      child: ListView.separated(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                    color: selectedService == index
+                        ? const Color(0xFF818AF9)
+                        : const Color(0xFFF6F6F6),
+                    border: selectedService == index
+                        ? Border.all(
+                            color: const Color(0xFFF1E5E5).withOpacity(.22),
+                            width: 2)
+                        : null,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                    child: Text(
+                  Service.all()[index],
+                  style: GoogleFonts.manrope(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: selectedService == index
+                          ? Colors.white
+                          : const Color(0xFF3F3F3F).withOpacity(.3)),
+                )),
+              ),
+          separatorBuilder: (context, index) => const SizedBox(
+                width: 10,
+              ),
+          itemCount: Service.all().length),
+    );
+  }
+
+  Widget _search() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      child: TextFormField(
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            prefixIcon: const Icon(
+              FeatherIcons.search,
+              color: Color(0xFFADACAD),
+            ),
+            hintText: "Find best vaccinate, treatment ...",
+            hintStyle: GoogleFonts.manrope(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xFFCACACA),
+                height: 150 / 100)),
+      ),
+    );
+  }
+
+  AspectRatio _card() {
+    return AspectRatio(
+      aspectRatio: 336 / 184,
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(14),
+          color: const Color(0xFF818AF9),
+        ),
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/background_card.png',
+              height: double.maxFinite,
+              width: double.maxFinite,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                      text: TextSpan(
+                          text: "Your ",
+                          style: GoogleFonts.manrope(
+                              fontSize: 14,
+                              color: const Color(0xFFDEE1FE),
+                              height: 150 / 100),
+                          children: const [
+                        TextSpan(
+                            text: "Catrine",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w800)),
+                        TextSpan(text: "will get\nvaccination"),
+                        TextSpan(
+                            text: "tomorrow \nat 07.00 am!",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w800)),
+                      ])),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(.4),
+                        border: Border.all(
+                            color: Colors.white.withOpacity(.12), width: 2),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(
+                      "See details",
+                      style: GoogleFonts.manrope(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
